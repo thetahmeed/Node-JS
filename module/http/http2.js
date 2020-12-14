3ea320f466306be670bbbef04b7e4ec05acfaddb
thetahmeed <tahmeed.com.bd@gmail.com>
2020-12-14 20:03:03 +0600

    added more

---
+++ module/http/http2.js
@@ -0,0 +1,30 @@
+
+
+
+// 2. getting response for a particular request
+
+var http = require('http');
+
+var server=http.createServer(function (req,res) {
+    
+    if (req.url == "/"){
+
+        res.writeHead(200, {'Content-Type' : 'text/html'})
+        res.write('<h1>Welcome to Home</h1>')
+        res.end();
+
+    }else if(req.url == "/chat"){
+
+        res.writeHead(200, {'Content-Type' : 'text/html'})
+        res.write('<h1>Welcome to ChatRoom</h1>')
+        res.end();
+
+    }
+    
+});
+
+server.listen(6969);
+
+// to test the response, browse: localhost:5050
+
+console.log("Server Run Successfully"); 
