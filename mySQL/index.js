893916c1e8300fe4c6a1eda9a31be42caab3a5d3
thetahmeed <tahmeed.com.bd@gmail.com>
2020-12-16 10:44:42 +0600

    how to insert data

--- mySQL/index.js
+++ mySQL/index.js
@@ -19,8 +19,29 @@ var databaseConnectionConfig ={
 var con = mSql.createConnection(databaseConnectionConfig);
 con.connect(function(err){
     if(err){
-        console.log("There is an ENAAAAAMI")
+        console.log("No database found")
     }else{
-        console.log("Come no aNAAMi")
+        console.log("Found a database")
+
+        // sending SQL conection 
+        insertData(con)
     }
 });
+
+// to insert data to the database
+function insertData(con){
+
+    // command for inserting data // you can find it on phmyadmin > SQL > insert
+    let SQL_COMMAND = "INSERT INTO `user_list`(`name`, `phone`) VALUES ('Afifa','01745241526')"
+
+    con.query(SQL_COMMAND, function(err){
+
+        if(err){
+            console.log("facing error to inserting data")
+        }else{
+            console.log("Data insered")
+        }
+
+    })
+
+}
