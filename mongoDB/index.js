9a0f8ced5edd1ddf49c8c5c55620e36c4f3d1c77
thetahmeed <tahmeed.com.bd@gmail.com>
2020-12-16 19:26:41 +0600

    Insert data

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -4,4 +4,52 @@
 
 
 
-console.log("Node is Working...")
+var mMongo = require('mongodb').MongoClient;
+// conecting with the hole database
+var mUrl = "mongodb+srv://tahmeedul:cK4H6VVF9fPQPYF6@cluster0.mvhfs.mongodb.net?retryWrites=true&w=majority";
+// connecting with a specific location
+// var mUrl = "mongodb+srv://tahmeedul:cK4H6VVF9fPQPYF6@cluster0.mvhfs.mongodb.net/user?retryWrites=true&w=majority";
+
+// configaration
+var mConfig = { useUnifiedTopology: true }
+
+// conecting to the database
+mMongo.connect(mUrl, mConfig, function(err, mongoClient){
+
+    if(err){
+        console.log("There is an error!")
+    }else{
+        console.log("Done!")
+        insertData(mongoClient);
+    }
+
+})
+
+// inserting data
+function insertData(mongoClient) {
+
+    // getting database
+    var myDB = mongoClient.db('user')
+
+    // getting table
+    var myDBTable = myDB.collection('user_list')
+
+    // this is the data
+    var myData = {
+        name: "Tahmeed",
+        phone: "01700547406",
+        country: "Bangladesh"
+    }
+
+    // pushing data to the database
+    myDBTable.insertOne(myData, function(err){
+
+        if(err){
+            console.log("Error!")
+        }else{
+            console.log("Data is added")
+        }
+
+    })
+
+}
